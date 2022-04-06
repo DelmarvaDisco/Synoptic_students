158b83fe04629db452f4ffae3fd8151d6ee056b3
James Maze <jtmaze@vt.edu>
2022-04-06 15:50:19 -0400

    relearning how maps work

--- James_M/GPS_data_Mar2022.R
+++ James_M/GPS_data_Mar2022.R
@@ -6,17 +6,39 @@
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # Notes:
-#
+# Just seeing if this works. Should I use leaflet or something else?
 
-# 1. Libraries and Packages -----------------------------------------------
+# 1. Packages and workspace -----------------------------------------------
 
 remove(list = ls())
-
-library(purrr) 
-library(tidyverse)
+ 
 library(leaflet)
 library(sf)
+library(raster)
 library(htmltools)
 library(htmlwidgets)
+library(tidyverse)
 
-data_dir <- "data/geospatial"
+#Define data directory
+data_dir <- "data/geospatial/"
+
+#Define master projection
+p <- "+proj=utm +zone=17 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"
+
+
+# 2. Read data -----------------------------------------------------
+
+#Read in the area shapefiles
+area_gen <- st_read(paste0(data_dir, "Area_gen.shp"))
+
+points_ge <- st_read(paste0(data_dir, "Point_ge.shp"))
+
+Generic <- st_read(paste0(data_dir, "Generic_.shp"))
+
+flooded_map <- leaflet(Generic) %>% 
+  addProviderTiles("Esri.WorldImagery", group = "ESRI")
+
+(flooded_map)
+
+
+
