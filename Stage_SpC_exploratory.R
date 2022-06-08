98247f74c6869f34ba0ca5bcbd61c1cf3b5f64d0
James Maze <jtmaze@vt.edu>
2022-06-08 11:33:44 -0400

    .

--- Stage_SpC_exploratory.R
+++ Stage_SpC_exploratory.R
@@ -19,30 +19,69 @@ data_dir <- "data/Stage_SpC/"
 
 # 2. Read in the data -----------------------------------------------------
 
-StageS21 <- read_csv(file = paste0(data_dir, "output_20210525_JM.csv"))
-StageF21 <- read_csv(file = paste0(data_dir, "output_20211112_JM.csv"))
-StageS22 <- read_csv(file = paste0(data_dir, "output_20220410_JM.csv"))
-
-#Combine stage and rename files
-Stage_data <- rbind(StageS21, StageF21, StageS22) %>% 
+Stage_data <- read_csv(file = paste0(data_dir, "Stage_data_dly_mean.csv")) %>% 
   rename("Site_ID" = Site_Name) %>% 
-  rename("Flag_stage" = Flag) %>% 
-  rename("Notes_stage" = Notes)
+  select(-c("version"))
 
-rm(StageS21, StageF21, StageS22)
-
-SpC_data <- read_csv(file = paste0(data_dir, "SpC_output_2021_2022.csv"))
+SpC_data <- read_csv(file = paste0(data_dir, "SpC_output_2021_2022.csv")) %>% 
+  mutate(Date = ymd(str_sub(Timestamp, 1, 10))) %>% 
+  group_by(Date, Site_ID) %>% 
+  summarize("SpC_low_range_dly" = mean(SpC_low_range),
+            "Temp_C_dly" = mean(Temp_C))
 
 # 3. Match the SpC data to Stage ------------------------------------------
 
 #Join data
-df <- left_join(SpC_data, Stage_data, by = c("Site_ID", "Timestamp"))
-#See SpC values missing stage values
-count_stage_nas <- df %>% filter(is.na(waterLevel))
+df <- full_join(SpC_data, Stage_data, by = c("Site_ID", "Date"))
 
-rm(SpC_data, Stage_data)
+#See SpC values missing stage values. Hopefully bring this number down overtime.
+count_stage_nas <- df %>% filter(is.na(dly_mean_wtrlvl))
+
+rm(SpC_data, Stage_data, count_stage_nas)
 
 # 4. Plot relationships ---------------------------------------------------
+
+
+# 4.1 Stage and SC at SW sites --------------------------------------------
+
+temp <- df %>% 
+  #Filter out the channel sites until further data QAQC descisions
+  filter(Site_ID %in% c("XB-SW", "OB-SW", "MB-SW", "HB-SW", 
+                         "TS-SW", "DK-SW", "ND-SW"))
+
+#Stage SpC relationships across sites
+stage_SpC <- ggplot(data = temp, 
+                    mapping = aes(x = dly_mean_wtrlvl,
+                                  y = SpC_low_range_dly,
+                                  color = Site_ID)) +
+  geom_point() +
+  scale_y_continuous(limits = c(0, 100)) +
+  scale_x_continuous(limits = c(0, 1.25)) +
+  theme_bw()
+
+(stage_SpC)
+
+# 4.2 Stage and SpC overtime at Baltimore Corner ---------------------------
+
+temp <- df %>% 
+  #Filter only BC surface water sites
+  filter(Site_ID %in% c("XB-SW", "OB-SW", "MB-SW", "HB-SW"))
+
+BC_stage_SpC <- ggplot(data = temp, 
+                       aes(x = Date,
+                           color = Site_ID)) +
+  geom_line(aes(y = dly_mean_wtrlvl * 100)) +
+  geom_point(aes(y = SpC_low_range_dly)) +
+  scale_y_continuous(name = "SpC_low_range",
+                     sec.axis = sec_axis(~./100, name = "Daily stage (m)")) +
+  scale_x_date(limits = as.Date(c("2021-09-21", "2022-05-02"))) +
+  theme_bw()
+
+(BC_stage_SpC)
+
+
+
+
 
 
 
