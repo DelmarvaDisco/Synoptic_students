53d77bd427536a065104ea905409fecebbc27d60
James Maze <jtmaze@vt.edu>
2022-06-16 12:36:38 -0400

    .

--- lab_data_aggregator.R
+++ lab_data_aggregator.R
@@ -6,7 +6,10 @@
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # Notes:
-# Drops some meta-data specific to analyses
+# - Drops some meta-data specific to analyses. Granularity sacrficed for maleability. 
+# - Eliminated all CBL data to avoid duplicates. If someone wants to do comps between CBL & VT,
+# then be my guest. 
+# _ 
 
 
 # 1. Libraries and Packages -----------------------------------------------
@@ -22,11 +25,9 @@ library(dplyr)
 
 data_dir <- "data/lab_data_combo/"
 
-
 # 2. Read in the data ----------------------------------------------------------------------
 
 # Download function for Isotopes, Nutrients, DOC (quantity), SO4-, Cl-
-
 download_fun1 <- function(file_paths){
   
   
@@ -53,28 +54,46 @@ download_fun1 <- function(file_paths){
   
 }
 
-####### Test space 
-
-
-
+#Isotope Data
 isotope_files <- list.files(paste0(data_dir, "Isotopes"), full.names = TRUE) 
 isotope_files <- isotope_files[!str_detect(isotope_files, "Precip")]
 
+#Anion Data
 anion_files <- list.files(paste0(data_dir, "Anions"), full.names = TRUE)
 
-NPOC_files <- 
+#NPOC Data
+NPOC_files <- list.files(paste0(data_dir, "DOC Concentration"), full.names = TRUE)
+NPOC_files <- NPOC_files[str_detect(NPOC_files, "NPOC")]
+#Remove the CBL data to eliminate duplicates
+NPOC_files <- NPOC_files[!str_detect(NPOC_files, "CBL")]
 
-file_paths <- c(anion_files, isotope_files)
-rm(anion_files, isotope_files)
+#Nutrient Data
+nutrient_files <- list.files(paste0(data_dir, "Nutrients"), full.names = TRUE)
+#Remove the CBL data to eliminate duplicates
+nutrient_files <- nutrient_files[!str_detect(nutrient_files, "CBL")]
 
+#Concatonate file paths
+file_paths <- c(anion_files, isotope_files, NPOC_files, nutrient_files)
+rm(anion_files, isotope_files, NPOC_files)
 
+#Run the download function
 df <- file_paths %>% 
   map(download_fun1) %>% 
   reduce(bind_rows)
 
+# 3. Read in the ICPMS metals data ----------------------------------------
 
-hmm <- df %>% 
-  filter(is.na(Units))
+
+# 4. Read in the GHG data ----------------------------------------------------
+
+
+# 5. Combine all the data -------------------------------------------------
+
+
+# 6. Reformat everything --------------------------------------------------
+
+
+# 7. Export to new csv --------------------------------------------------------
 
 
 
